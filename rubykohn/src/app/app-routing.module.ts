2bc2ba307391751a68fb0cbf1a1f14fcb5890178
rubykohnsite <rubykohnsite@gmail.com>
2019-02-19 21:10:07 -0500

    routing sans content & styling

--- rubykohn/src/app/app-routing.module.ts
+++ rubykohn/src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent } from './home/home.component';
+import { ResearchComponent } from './research/research.component';
+import { ContactComponent } from './contact/contact.component';
+
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'research', component: ResearchComponent },
+  { path: 'contact', component: ContactComponent },
+];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- rubykohn/src/app/app.module.ts
+++ rubykohn/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ContactComponent } from './contact/contact.component';
+import { ResearchComponent } from './research/research.component';
+import { NavComponent } from './nav/nav.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ContactComponent,
+    ResearchComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,
